3bf0e54c9c8128373d3e5bc524b0e2aca8e13323
Mohammad Reza Kazemi <cleverboy02.01@gmail.com>
2025-10-14 16:31:25 +0330

    Add GitHub Pages deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/english-institute/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
